5611df06a0b38b6e2c4fd4aa3807aa6c372b06d1
Nick <nick731@gmail.com>
2021-02-23 02:18:22 +0800

    add tf

---
+++ template/main.tf
@@ -0,0 +1,52 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "infra-uksouth-rg"
+  location = "westus2"
+}
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "azureteststoragescbtest"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_share" "example" {
+  name                 = "sharename"
+  storage_account_name = azurerm_storage_account.example.name
+  quota                = 50
+
+  acl {
+    id = "MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI"
+
+    access_policy {
+      permissions = "rwdl"
+      start       = "2019-07-02T09:38:21.0000000Z"
+      expiry      = "2019-07-02T10:38:21.0000000Z"
+    }
+  }
+}
+
+
+resource "azurerm_storage_share_directory" "example" {
+  name                 = "examplescbtest"
+  share_name           = azurerm_storage_share.example.name
+  storage_account_name = azurerm_storage_account.example.name
+}
+
+
